2b770a4d58d51443f439bdf5ab0a2cf7652d65c2
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-01-18 12:56:04 -0300

    infrastructure configuring with aws

---
+++ aws/infrastructure/create_infrastructure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+## Create Cloudformation stack
+echo "Create cloudformation infrastructure"
+../create.sh udacity-capstone-infra infrastructure.yml infrastructure_parameters.json us-west-2
---
+++ aws/infrastructure/delete_infrastructure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+## Delete Cloudformation stack
+echo "Delete cloudformation infrastructure"
+../delete.sh udacity-capstone-infra us-west-2
