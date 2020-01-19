0d855a774ac9422f47aa57b5d42979b0cec9a8ae
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-01-19 15:49:43 -0300

    Rename directory

---
+++ project/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Udacity-DevOpsEnginner-Capstone for Cloud DevOps Enginner</p>
+        <p>Developed by 
+          <a
+            className="App-link"
+            href="https://github.com/fabiotavarespr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @fabiotavarespr
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
