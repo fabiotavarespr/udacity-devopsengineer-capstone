699270490a8f22df1feaafed68317022ee60e9c6
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-01-19 11:24:40 -0300

    Fixing split line

--- aws/create_with_iam.sh
+++ aws/create_with_iam.sh
@@ -5,5 +5,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=$4 /
+--region=$4 \
 --capabilities CAPABILITY_IAM
--- aws/update_with_iam.sh
+++ aws/update_with_iam.sh
@@ -5,5 +5,5 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=$4 /
+--region=$4 \
 --capabilities CAPABILITY_IAM
